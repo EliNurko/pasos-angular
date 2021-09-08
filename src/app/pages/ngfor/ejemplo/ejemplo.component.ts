29ab05c954f7668c1df7dfc904a06c6bb4e5b1f2
Eli Nurko <eli@mangos77.mx>
2021-09-08 15:15:49 -0500

    Se termino de hacer los archivos de ngFor

--- src/app/pages/ngfor/ejemplo/ejemplo.component.ts
+++ src/app/pages/ngfor/ejemplo/ejemplo.component.ts
@@ -9,6 +9,13 @@ export class EjemploComponent implements OnInit {
 
   peliculas:string [] = ["Star Wars", "Avengers", "S.W.A.T.", "Rápidos y furiosos"]
 
+  coches: Array<any> = [
+    {tipo:'Deportivo', marca:'Ford', cantidad:5},
+    {tipo:'Super deportivo', marca:'Ferrari', cantidad:3},
+    {tipo:'Lujo', marca:'Bentley', cantidad:8}
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
